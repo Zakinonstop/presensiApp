c4c2b3172a35fe442a33d59a2cc8c48a97aa6b03
Zakinonstop <zakinonstop1@gmail.com>
2021-07-06 11:00:20 +0700

    modifikasi view layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,13 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                
+                <div>
+                    <nav-link>
+                        Jadwal
+                    </nav-link>
+                </div>
+
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>PresensiKu</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PresensiKu
+                </div>
+
+                <div class="sub-title m-b-md">
+                  Dibuat oleh :  Muhammad Zakiyuddin | Panji RIyadi | Tubagus Dhimas
                 </div>
 
                 <div class="links">
